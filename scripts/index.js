6922b12e5894e21288e5707be44710009794bfc6
Ekaterina Epiphanova <epiph@yandex.ru>
2022-03-26 23:52:37 +0500

    refactor: write openPopup function

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
 let openButton = document.querySelector(".profile__edit-button");
 let modalWindow = document.querySelector(".popup");
 let closeButton = modalWindow.querySelector(".popup__close-button");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let editForm = modalWindow.querySelector(".popup__form");
+let nameInput = modalWindow.querySelector(".popup__input_type_name");
+let jobInput = modalWindow.querySelector(".popup__input_type_job");
+let submitButton = modalWindow.querySelector(".popup__submit-button");
 
 function toggleModalWindow() {
   modalWindow.classList.toggle("popup_opened");
 }
 
-openButton.addEventListener("click", toggleModalWindow);
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  toggleModalWindow();
+}
+
+openButton.addEventListener("click", openPopup);
 
 closeButton.addEventListener("click", toggleModalWindow);
-
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
-let editForm = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input_type_name");
-let jobInput = document.querySelector(".popup__input_type_job");
-let submitButton = modalWindow.querySelector(".popup__submit-button");
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
 function editProfile(evt) {
   evt.preventDefault();
