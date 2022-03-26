c612ec90ed53ac6a15228ba1781b5b0dd03787c5
Ekaterina Epiphanova <epiph@yandex.ru>
2022-03-26 17:36:12 +0500

    feat: write popup and edit-form functions

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,24 @@ function toggleModalWindow() {
 openButton.addEventListener("click", toggleModalWindow);
 
 closeButton.addEventListener("click", toggleModalWindow);
+
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let editForm = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+let submitButton = modalWindow.querySelector(".popup__submit-button");
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
+
+function editProfile(e) {
+  e.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  toggleModalWindow();
+}
+
+editForm.addEventListener("submit", editProfile);
+
+// submitButton.addEventListener("click", toggleModalWindow);
