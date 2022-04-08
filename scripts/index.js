d92847a1f4dfdfa21c08182438df818fd4f76965
Ekaterina Epiphanova <epiph@yandex.ru>
2022-04-08 18:12:54 +0500

    feat: load initial set of elements__items in js

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,53 @@ function editProfile(evt) {
 }
 
 editForm.addEventListener("submit", editProfile);
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const elementsContainer = document.querySelector(".elements");
+const template = document.querySelector(".elements__template");
+
+function getElement(item) {
+  const elementTemplate = template.content.cloneNode(true);
+  const title = elementTemplate.querySelector(".elements__title");
+  const image = elementTemplate.querySelector(".elements__photo");
+
+  title.textContent = item.name;
+  image.src = item.link;
+  image.alt = item.mame;
+
+  return elementTemplate;
+}
+
+function getElementsList(arr) {
+  const elementsList = arr.map(getElement);
+  elementsContainer.append(...elementsList);
+  return elementsContainer;
+}
+
+getElementsList(initialCards);
